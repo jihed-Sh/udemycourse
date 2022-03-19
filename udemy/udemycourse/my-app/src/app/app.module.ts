935cc16b0e65e1b0375b3aeb1597eaf3eb76864a
jihed.benzarb <jihed.benzarb@eniso.u-sousse.tn>
2022-03-20 00:30:34 +0100

    added a header

--- udemy/udemycourse/my-app/src/app/app.module.ts
+++ udemy/udemycourse/my-app/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HeaderComponent } from './header/header.component';
+import { PostComponent } from './post/post.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { PostListComponent } from './post-list/post-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HeaderComponent,
+    PostComponent,
+    PostEditComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule
