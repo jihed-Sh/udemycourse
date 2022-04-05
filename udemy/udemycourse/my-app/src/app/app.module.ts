e536b810b2f3daff13e4fd4312cacb5a471e41c7
jihed.benzarb <jihed.benzarb@eniso.u-sousse.tn>
2022-04-05 02:36:48 +0200

    [UPDATED]

--- udemy/udemycourse/my-app/src/app/app.module.ts
+++ udemy/udemycourse/my-app/src/app/app.module.ts
@@ -7,7 +7,29 @@ import { HeaderComponent } from './header/header.component';
 import { PostComponent } from './post/post.component';
 import { PostEditComponent } from './post-edit/post-edit.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+
+const routes: Routes=[
+  {
+    path:"",
+    redirectTo:"/post-list",
+    pathMatch:"full"
+  },
+  {
+    path:"post-list",
+    component:PostListComponent,
+  },
+  {
+    path:"post-add",
+    component:PostEditComponent, 
+  },
+  {
+    path:"auth",
+    component:AuthComponent,
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,7 +40,8 @@ import { PostListComponent } from './post-list/post-list.component';
     PostListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
